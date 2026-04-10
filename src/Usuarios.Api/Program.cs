6fa1619e107d7151790cd9c8e9e22a06dced6b58
Felipe Augusto <felipementel@hotmail.com>
2026-04-10 18:13:38 -0300

    feat: example devsecops

--- src/Usuarios.Api/Program.cs
+++ src/Usuarios.Api/Program.cs
@@ -5,12 +5,19 @@ using Usuarios.Domain.Ports;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddOpenApi(options =>
+{
+    // Specify the OpenAPI version to use
+    options.OpenApiVersion = Microsoft.OpenApi.OpenApiSpecVersion.OpenApi3_0;
+});
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<IUserRepository, InMemoryUserRepository>();
 builder.Services.AddSingleton<UserService>();
 
 var app = builder.Build();
+
+app.MapOpenApi("/openapi/{documentName}.yaml");
 
 app.UseSwagger();
 app.UseSwaggerUI();
